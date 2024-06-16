a225b7e02e7828c22e85239e78670fd4f594af96
The_Ultimate_Sequeira <117172083+Nathan-sudo-pycharm@users.noreply.github.com>
2024-06-16 15:20:03 +0530

    commit 4

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -17,13 +17,14 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
+              Hello, I&apos;m
             </span>
             <br></br>
+            Vinston Cutinha
+            <br></br>
+            {/* typing animation 
             <TypeAnimation
               sequence={[
-                "Judy",
-                1000,
                 "Web Developer",
                 1000,
                 "Mobile Developer",
@@ -34,7 +35,7 @@ const HeroSection = () => {
               wrapper="span"
               speed={50}
               repeat={Infinity}
-            />
+            /> */}
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
@@ -42,17 +43,13 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
-            >
-              Hire Me
-            </Link>
-            <Link
-              href="/"
+              href="/my_cv.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              target="_blank"
+              download="my_cv.pdf"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+                Download My CV
               </span>
             </Link>
           </div>
